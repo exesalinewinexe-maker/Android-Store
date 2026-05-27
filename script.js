6d4121f96fafd62fb4ca96707063453108ed4c43
exesalinewinexe-maker <exesalinewin.exe@gmail.com>
2026-05-26 17:55:24 -0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Register the service worker (simple, no skip button)
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .catch(err => console.log('SW registration failed:', err));
-}
